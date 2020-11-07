1586dc73f36b04159b4ad5e25c3a3e47db39fb77
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-06 20:44:34 -0500

    added content to scores js to try and render the saved initials. currently broken and working on a fix

---
+++ assets/scores-script.js
@@ -0,0 +1,20 @@
+var list = document.getElementById("scoresList");
+
+var scores = [];
+
+function initialStoragePull() {
+    var storedScores = JSON.parse(localStorage.getItem("scores"));
+
+    if (storedScores !== null) {
+        return
+    }
+
+    for (let i=0; i< storedScores.length; i++) {
+        var listItem = document.createElement("li")
+        listItem.textContent = storedScores[i]
+        list.appendChild(listItem)
+    }
+}
+
+
+initialStoragePull();
